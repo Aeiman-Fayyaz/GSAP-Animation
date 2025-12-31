28f5ab13d805364b2aba53b88a9cb20c17d99181
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-12-31 15:02:05 +0500

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,58 @@
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import { useGSAP } from "@gsap/react";
-import gopi from "./assets/gopi-bahu.jpg"
-import ahm from "./assets/ahm-g.avif"
+import gopi from "./assets/gopi-bahu.jpg";
+import ahm from "./assets/ahm-g.avif";
 
 export default function App() {
+  //   useGSAP(() => {
+  //     gsap.to(".data", {
+  //       x: 1250,
+  //       duration: 5,
+  //       delay: 1,
+  //       rotate: 720,
+  //     });
+  //   });
   useGSAP(() => {
-    gsap.to(".data", {
-      x: 1250,
-      duration: 5,
+    gsap.to(".gopi", {
+      x: 1200,
+      duration: 10,
       delay: 1,
+      repeat: -1,
       rotate: 720,
+      rotateY: 450,
+    });
+    gsap.from(".ahm", {
+      x: 1000,
+      duration: 10,
+      delay: 1,
+      repeat: -1,
+      rotate: 720,
+      rotateY: 450,
     });
   });
-  useGSAP(()=>{
-    gsap.to(".gopi" ,{
-        x: 1200,
-        duration: 10,
-        delay: 1,
-        repeat: -1,
-        rotate: 720,
-        rotateY: 450
-    })
-    gsap.from(".ahm" ,{
-        x: 1000,
-        duration: 10,
-        delay: 1,
-        repeat: -1,
-        rotate: 720,
-        rotateY: 450
-    })
-  })
+
+  //   Animation by using useRef
+  // Create Reference
+  const eleRef = useRef();
+
+  useRef(() => {
+    gsap.to(eleRef.current , {
+      x: 1000,
+      duration: 5,
+      delay: 2,
+      rotate: 720
+    });
+  });
 
   return (
     <>
-      {/* <div className="data"></div> */}
+      <div ref={eleRef} className="data"></div>
       <div>
         <img src={gopi} className="gopi" />
       </div>
       <div>
-
         <img src={ahm} className="ahm" />
       </div>
     </>
